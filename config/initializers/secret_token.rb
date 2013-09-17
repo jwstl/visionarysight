ac05c596e61a94841092dd5f72e957bc1fe7091f
jason white <info@turn2.co>
2013-09-16 20:55:14 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visionarysight::Application.config.secret_token = '07f2e9546bccbf202632c30265a812e20251a9842fb2ec18698e3ffaefa55d0d71116d275760387ec810ecb469f40b77932f84166f39caf108f29a2126c00fdc'
